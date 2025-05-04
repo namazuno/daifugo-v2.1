f0d3f60f4aae6c789b5136811ae7d889de26d5ea
namazuno <black4142kuri@gmail.com>
2025-05-04 16:19:59 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-// server.js for daifugo v2.6 - fixed
+
+        const express = require("express");
+        const socketIO = require("socket.io");
+
+        const app = express();
+        const server = app.listen(3000, () => {
+            console.log("Server running on port 3000");
+        });
+
+        const io = socketIO(server);
+
+        let players = [];
+
+        io.on("connection", (socket) => {
+            console.log("a user connected");
+
+            socket.on("joinGame", (name) => {
+                players.push({ id: socket.id, name });
+                io.emit("updatePlayers", players);
+            });
+
+            socket.on("startTurn", (turnPlayerId) => {
+                const turnPlayer = players.find(p => p.id === turnPlayerId);
+                if (turnPlayer) {
+                    io.emit("turnStarted", turnPlayer);
+                }
+            });
+
+            socket.on("disconnect", () => {
+                console.log("a user disconnected");
+                players = players.filter(p => p.id !== socket.id);
+                io.emit("updatePlayers", players);
+            });
+        });
+    
